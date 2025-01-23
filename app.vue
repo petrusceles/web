a86f043f759a492f1b604f398c18403aab6d3ed9
petrusceles <petrusprihantoro@gmail.com>
2025-01-23 18:02:51 +0700

    feat: remove loading

--- app.vue
+++ app.vue
@@ -25,16 +25,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <div
+  <!-- <div
     v-if="isLoading"
     ref="loadingSelector"
     class="font-bold fixed top-1/2 left-1/2 z-50 -translate-x-1/2 translate-y-1/2"
   >
     Loading... Please hold on, great things are on their way!
-  </div>
-  <template v-else>
-    <NuxtLayout> <NuxtPage /> </NuxtLayout>
-  </template>
+  </div> -->
+  <!-- <template v-else> -->
+  <NuxtLayout> <NuxtPage /> </NuxtLayout>
+  <!-- </template> -->
 </template>
 
 <style></style>
